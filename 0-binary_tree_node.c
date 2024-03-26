7aca8764e3590664f15de447b2debd89f6c3b040
Mohammed <mrcloud.med@gmail.com>
2024-03-26 21:55:23 +0000

    Update Create Node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,7 +13,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     binary_tree_t *new_tree;
 
     new_tree = malloc(sizeof(binary_tree_t));
-
+    if (new_tree == NULL)
+        return (NULL);
+    
     new_tree->parent = parent;
     new_tree->n = value;
     new_tree->left = NULL;
