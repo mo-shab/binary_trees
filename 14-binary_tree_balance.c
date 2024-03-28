006cea358495b9f974163081a61e2125bc19da9a
Mohammed <mrcloud.med@gmail.com>
2024-03-28 03:06:45 +0000

    Balnce

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,8 +11,11 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    
-    
+	if (tree == NULL)
+		return (0);
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return ((int)(left_height - right_height));
 }
