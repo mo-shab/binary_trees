1260e36570f5bd8cf235c45a14577b3763ba384a
Mohammed <mrcloud.med@gmail.com>
2024-03-28 12:17:29 +0000

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,14 +9,10 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	{
 		return (NULL);
-
-	binary_tree_t *grandparent = node->parent->parent;
-
-	if (grandparent->left == node->parent)
-		return (grandparent->right);
-	else if (grandparent->right == node->parent)
-		return (grandparent->left);
-	else
-		return (NULL);
+	}
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
