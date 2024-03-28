3edd3ea7e2053c6dcc0b84aebeea82c02eaf6a64
Mohammed <mrcloud.med@gmail.com>
2024-03-28 03:15:48 +0000

    Update balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,8 +14,28 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	int left_height = _binary_tree_height(tree->left) + 1;
+	int right_height = _binary_tree_height(tree->right) + 1;
 
-	return ((int)(left_height - right_height));
+	return (left_height - right_height);
+}
+
+
+/**
+* _binary_tree_height - Function that measures the height of a binary tree.
+* @tree: The binary tree.
+*
+* Return: the height of the tree, or 0 if tree is NULL.
+*/
+size_t _binary_tree_height(const binary_tree_t *tree)
+{
+	int left = 0, right = 0;
+
+	if (tree == NULL)
+		return (0);
+
+left = 1 + _binary_tree_height(tree->left);
+right = 1 + _binary_tree_height(tree->right);
+
+	return ((left > right) ? left : right);
 }
