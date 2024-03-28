3a705f8aa21d100a78ce90e2a566dd89e1bbfcdc
Mohammed <mrcloud.med@gmail.com>
2024-03-28 23:28:16 +0000

    ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,8 +8,23 @@
  * Return: pointer of the lowest common ancestor of the two nodes.
  * if no common found, return NULL.
 */
-binary_tree_t *binary_trees_ancestor(
-	const binary_tree_t *first, const binary_tree_t *second)
-{
 
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	binary_tree_t *p, *q;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	p = first->parent;
+	q = second->parent;
+	if (p == NULL || first == q || (!p->parent && q))
+		return (binary_trees_ancestor(first, q));
+	else if (q == NULL || p == second || (!q->parent && p))
+		return (binary_trees_ancestor(p, second));
+	return (binary_trees_ancestor(p, q));
 }
